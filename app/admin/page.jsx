6f6854bc0dd319799fb51ee4b1bff6443783bddb
Ivan Kohan <kohan3750@gmail.com>
2025-07-13 23:00:28 +0200

    cms

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { NextStudio } from 'next-sanity/studio'
-import config from '../../app/cms/sanity.config.js' // Шлях до studio config
+import config from '../../app/cms/sanity.config.js'
 
 export default function AdminPage() {
   return <NextStudio config={config} />
